de0438637b3768e3c26004d4964b6863f9e2792e
Vineet Markan <vineet.markan@gmail.com>
2021-06-27 17:31:15 +0530

    remove shameless branding plug

--- templates.js
+++ templates.js
@@ -5,7 +5,6 @@ const kuberUIBox = (data) => {
     <div id='kuber-box'>
         <div class='kuber-branding'>
             <span class='logo'> KUBER </span>
-            <span class='personal'> by Vineet </span>
         </div>
         <div class='kuber-insights'> ${content} </div>
     </div >
