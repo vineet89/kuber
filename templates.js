278b57128a26aabafa8dbdc7fb905d1fc976249f
Vineet Markan <vineet.markan@gmail.com>
2021-06-28 08:43:14 +0530

    styling changes - left align list

--- templates.js
+++ templates.js
@@ -1,12 +1,12 @@
 const kuberUIBox = (data) => {
     let content = '';
-    data.forEach((value, key) => content += `<p> ${key + ' ' + value} </p>`)
+    data.forEach((value, key) => content += `<li> ${key + ' ' + value} </li>`)
     return `
     <div id='kuber-box'>
         <div class='kuber-branding'>
             <span class='logo'> KUBER </span>
         </div>
-        <div class='kuber-insights'> ${content} </div>
+        <ul class='kuber-insights'> ${content} </ul>
     </div >
     `
 };
